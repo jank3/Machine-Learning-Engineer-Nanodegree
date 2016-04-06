b663fd3d60e518a94584527baa3e1ce7d4dac33d
mikecassell <mike.cassell@rogers.com>
2016-04-05 20:05:39 -0400

    Fixed the algorithm bug and scaled testing
    To really be able to generalize about the performance of differing Alpha and Gamma parameters, I need a bigger sample of training data.

--- P4. Train a Smartcab/smartcab/smartcab/agent.py
+++ P4. Train a Smartcab/smartcab/smartcab/agent.py
@@ -5,6 +5,7 @@ from planner import RoutePlanner
 from simulator import Simulator
 from collections import namedtuple
 import pandas as pd
+import os.path
 
 stateRecord = namedtuple('stateRecord', ['light','opposing','nextWaypoint'])
 validActions = [None, 'forward', 'left', 'right']
@@ -32,21 +33,34 @@ class LearningAgent(Agent):
         self.gamma = 0.3
         self.explore = 0.99
         self.stateHist = {}
-        self.counter = 0
-        self.rewards = 0
-        self.trip = 1
+        self.run = 1        # Iteration counter for each alpha/gamma change
+        self.trip = 0       # Trip counter in each iteration
+        self.counter = 0    # Step counter in each run
+        self.glNum = 0      # global counter
+        self.rewards = 0 
+        self.experiment = 'None'
         
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        if self.counter > 0:
+        self.trip += 1      # Increment the trip
+        self.counter = 0    # Reset the counter for the next run
+        print('Trip:', self.trip)
+            
+        if self.trip == 100:
+            hasHist = False            
+            if os.path.isfile('runHist.pkl'):
+                temp = pd.read_pickle('runHist.pkl')
+                hasHist = True
+
             df = pd.DataFrame(self.stateHist).T
-            df.columns = ['trip','light','opposing','oncoming','left','right','next_waypoint',
-                          'action', 'qA', 'qV', 'reward', 'alpha', 'deadline','Explored']            
+            df.columns = ['experiment','batch','trip','counter', 'light','opposing','oncoming','left','right','next_waypoint',
+                          'action', 'qA', 'qV', 'reward', 'alpha', 'gamma', 'deadline','Explored']
+            if hasHist:
+                df = pd.concat([df, temp])
             df.to_pickle('runHist.pkl')
-            self.trip += 1
-            self.counter = 0
-                    
+            self.stateHist = {}
+        
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
@@ -68,13 +82,12 @@ class LearningAgent(Agent):
         
         
         # TODO: Select action according to your policy  
-        
         qA, qV = argMax(self.Q, self.state)
         choseExpl = False
         if random.randrange(0, 100)/100.0 < self.explore or qV == 0:
             action = validActions[random.randint(0,3)]
             if self.explore > 0.00:
-                self.explore = self.explore  - 0.01
+                self.explore = self.explore - 0.01
             choseExpl = True
         else:
             action = qA
@@ -89,34 +102,56 @@ class LearningAgent(Agent):
             
         s = stateRecord(rInputs['light'], nOpposing, self.next_waypoint)
         
-        self.Q[self.state][action] = self.alpha * (reward + self.gamma * argMax(self.Q, s)[1])
+        self.Q[self.state][action] = self.Q[self.state][action] * (1- self.alpha) + self.alpha * (reward + self.gamma * argMax(self.Q, s)[1])
         
         writeout = 'Lights:{} Goal:{} '.format( str(self.state[0]), str(self.state[2])) 
         writeout += 'Action:{} Reward:{} Qest:{} Expl:{} Exp:{}'.format(str(action), str(reward), str(self.gamma * argMax(self.Q, s)[1]), choseExpl, self.explore)
         
-        print(writeout)
-        
+        #print(writeout)
         self.rewards += reward
-        self.stateHist[self.trip * 1000 + self.counter] = [self.trip, 
+        n = str(self.glNum) +'.' + str(self.trip) + '.' + str(self.counter)
+        self.stateHist[n] = [self.experiment, self.glNum ,self.trip, self.counter,  
                             inputs['light'], opposing, inputs['oncoming'], inputs['left'], 
                             inputs['right'], self.next_waypoint, 
-                            action, qA, qV, reward, self.gamma, deadline, choseExpl]
+                            action, qA, qV, reward, self.alpha, self.gamma, deadline, choseExpl]
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}, step = {}".format(deadline, inputs, action, reward, self.counter)  # [debug]
 
 
 def run():
     """Run the agent for a finite number of trials."""
-
-    # Set up environment and agent
-    e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
-
-    # Now simulate it
-    sim = Simulator(e, update_delay=0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  # press Esc or close pygame window to quit
-
+    optimize = True
+    globalNum = 0
+    if optimize:
+        for alpha in range(1, 16):
+            for n in range(1,21):
+                e = Environment()  # create environment (also adds some dummy traffic)
+                a = e.create_agent(LearningAgent)  # create agent
+                a.experiment  ='alpha'
+                e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
+                sim = Simulator(e, update_delay=0)  # reduce update_delay to speed up simulation
+                a.glNum = globalNum
+                globalNum += 1
+                a.alpha = alpha / 20.0
+                a.run += 1
+                a.counter = 0
+                a.trip = 0
+                sim.run(n_trials=100) 
+            
+        for gamma in range(1, 16):
+            for n in range(1,21):
+                e = Environment()  # create environment (also adds some dummy traffic)
+                a = e.create_agent(LearningAgent)  # create agent
+                a.experiment  ='gamma'
+                e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
+                sim = Simulator(e, update_delay=0)  # reduce update_delay to speed up simulation
+                a.glNum = globalNum
+                globalNum += 1
+                a.gamma = gamma / 20.0
+                a.run += 1
+                a.counter = 0
+                a.trip = 0
+                sim.run(n_trials=100)
 
 if __name__ == '__main__':
     run()
